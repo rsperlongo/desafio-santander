35788880375d96881fbcafaf666bab20dbd332e1
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-07-08 16:22:23 -0300

    update parte2

--- src/app/parte2/parte2.component.ts
+++ src/app/parte2/parte2.component.ts
@@ -17,20 +17,30 @@ export class Parte2Component implements OnInit {
   createChart() {
     this.chart = new Chart("MyChart", {
       type: 'doughnut',
+      options: {
+        cutout:100,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              textAlign: 'center'
+            },
+          },
+        }
+      },
       data: {
         labels: [
-          'Blue',
-          'Marcações Concluídas',
+          '% Marcações Parciais',
+          '% Marcações Completas',
         ],
         datasets: [{
           data: [70, 200],
           backgroundColor: [
             'rgb(195, 198, 198)',
             'rgb(78, 192, 221)',
-            
-            
           ],
-          hoverOffset: 2
+          hoverOffset: 2,
         }]
       }  
     })
